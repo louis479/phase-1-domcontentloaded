4a6c348b57e20e33d8941a73c1f56c267c51e71b
louis-ngugi <louisngugi858@gmail.com>
2024-12-13 20:01:28 +0300

    created a DOM Content Loaded in index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+// JavaScript code to meet the test requirements
+
+document.addEventListener("DOMContentLoaded", () => {
+    const p = document.querySelector("p");
+    if (p) {
+        p.textContent = "This is really cool!";
+    }
+});
